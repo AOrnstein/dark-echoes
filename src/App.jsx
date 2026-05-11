f329771da5234e7fae177f9bed2ecf427f7ed44b
AOrnstein <ornstein.adam@gmail.com>
2026-05-11 10:14:59 -0600

    build App functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,54 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+
 export default function App() {
-  // TODO
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  /** Detailded information about the selected episode */
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <p>Select a puppy to learn more.</p>
+        </section>
+      );
+    }
+
+    return (
+      <section className="details">
+        <h2>{selectedEpisode.title}</h2>
+        <p>{selectedEpisode.description}</p>
+      </section>
+    );
+  }
+
+  /** List of episodes that the user can select from */
+  function EpisodeList() {
+    return (
+      <section className="roster">
+        <h2>Episodes</h2>
+        <ul className="roster">
+          {episodes.map((episode) => (
+            <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+              {episode.title}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Dark Echos</h1>
+      </header>
+      <main>
+        <EpisodeList />
+        <EpisodeDetails />
+      </main>
+    </>
+  );
 }
